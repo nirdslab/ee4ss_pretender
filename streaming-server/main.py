0bdad107d79e47e00d76cfa81d419f13bbcd79f0
yasith <yasith.jayawardana@icloud.com>
2021-02-25 02:01:06 -0500

    update script with multi-client support

--- streaming-server/main.py
+++ streaming-server/main.py
@@ -14,78 +14,80 @@ logger.level = logging.DEBUG
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
-class Stream:
-    def __init__(self, l: str, t: str, fr: float, val: list, paused: bool = True):
-        self.label = l
-        self.type = t
-        self.freq = fr or 0.1
-        self.value = val
-        self.last_send = 0.0
-        self.paused = paused
-
-    async def read(self):
-        while True:
-            t = now()
-            if not self.paused and (t - self.last_send) > 1 / self.freq:
-                self.last_send = t
-                yield f"{self.label} {t} {' '.join(map(str, self.value))}"
-            await asyncio.sleep(1 / self.freq)
-
-    def pause(self):
-        self.paused = True
-
-    def resume(self):
-        self.paused = False
-
-
-class StreamHolder:
-
-    def __init__(self) -> None:
-        self.streams: Dict[str, Stream] = {
-            "acc": Stream("E4_Acc", "acc", 32, [100, 200, 300]),
-            "bvp": Stream("E4_Bvp", "bvp", 64, [400]),
-            "ibi": Stream("E4_Ibi", "ibi", 0, [500]),
-            "gsr": Stream("E4_Gsr", "gsr", 4,  [600]),
-            "tmp": Stream("E4_Temperature", "tmp", 4, [700]),
-            "hr": Stream("E4_Hr", "hr", 0, [800]),
-            "bat": Stream("E4_Bat", "bat", 0, [1.0]),
-            "tag": Stream("E4_Tag", "tag", 0, [1000])
-        }
-        self.is_paused = False
-
-    def resume_all(self):
-        for stream_id in self.streams:
-            self.streams[stream_id].resume()
-        self.is_paused = False
-
-    def pause_all(self):
-        for stream_id in self.streams:
-            self.streams[stream_id].pause()
-        self.is_paused = True
-
-    def paused(self) -> bool:
-        return self.is_paused
-
-    @staticmethod
-    async def __task__(s: Stream, w: StreamWriter):
-        async for data in s.read():
-            w.write(f"{data}\r\n".encode())
-
-    def new_streaming_loop(self, w: StreamWriter):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        tasks = []
-        for stream_id in self.streams:
-            tasks.append(loop.create_task(self.__task__(self.streams[stream_id], w)))
-        loop.run_forever()
-
-
 def now():
     '''Helper function to generate timestamps in EE4 format'''
     return datetime.now().timestamp()
 
 
-def cmd_handler(msg: str) -> str:
+class Stream:
+    def __init__(self, l: str, t: str, fr: float, val: list):
+        self.label = l
+        self.type = t
+        self.freq = fr or 0.1
+        self.value = val
+        self.last_t = 0.0
+        self.last_v = None
+
+    def read_latest(self):
+        t = now()
+        if (t - self.last_t) > 1 / self.freq:
+            self.last_t = t
+            self.last_v = ' '.join(map(str, self.value))
+        return f"{self.label} {self.last_t} {self.last_v}"
+
+
+STREAM_TYPES = ["acc", "bvp", "gsr", "ibi", "tmp", "bat", "tag", "hr"]
+STREAMS: Dict[str, Stream] = {
+    "acc": Stream("E4_Acc", "acc", 32, [100, 200, 300]),
+    "bvp": Stream("E4_Bvp", "bvp", 64, [400]),
+    "ibi": Stream("E4_Ibi", "ibi", 0, [500]),
+    "gsr": Stream("E4_Gsr", "gsr", 4,  [600]),
+    "tmp": Stream("E4_Temperature", "tmp", 4, [700]),
+    "hr": Stream("E4_Hr", "hr", 0, [800]),
+    "bat": Stream("E4_Bat", "bat", 0, [1.0]),
+    "tag": Stream("E4_Tag", "tag", 0, [1000])
+}
+
+
+class SubscriptionContext:
+    def __init__(self) -> None:
+        self.subscribed_to = []
+        self.paused = False
+        self.keepalive = True
+
+    def subscribe(self, name: str):
+        if name not in self.subscribed_to:
+            self.subscribed_to.append(name)
+        else:
+            logger.warn(f'attempt to resubscribe to {name}')
+
+    def unsubscribe(self, name: str):
+        if name in self.subscribed_to:
+            self.subscribed_to.remove(name)
+        else:
+            logger.warn(f'attempt to unsubscribe from non-subscribed {name}')
+
+    def end(self):
+        self.keepalive = False
+
+    async def __task__(self, stream_id: str, w: StreamWriter):
+        s = STREAMS[stream_id]
+        while self.keepalive:
+            if not self.paused and s.type in self.subscribed_to:
+                data = s.read_latest()
+                w.write(f"{data}\r\n".encode())
+            await asyncio.sleep(1 / s.freq)
+
+    def create_streaming_loop(self, w: StreamWriter):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        tasks = []
+        for stream_id in STREAMS:
+            tasks.append(loop.create_task(self.__task__(stream_id, w)))
+        loop.run_until_complete(asyncio.gather(*tasks, loop=loop, return_exceptions=True))
+
+
+def cmd_handler(msg: str, ctx: SubscriptionContext) -> str:
     # If msg does not exist, or cmd empty, return empty
     if not msg:
         return ""
@@ -104,6 +106,15 @@ def cmd_handler(msg: str) -> str:
             return "R device_subscribe ERR too many arguments"
         sub = args[0].lower()
         if sub in STREAM_TYPES:
+            ctx.subscribe(sub)
+            return f"R {cmd} {sub} OK"
+
+    if (cmd == "device_unsubscribe"):
+        if len(args) > 2:
+            return "R device_unsubscribe ERR too many arguments"
+        sub = args[0].lower()
+        if sub in STREAM_TYPES:
+            ctx.unsubscribe(sub)
             return f"R {cmd} {sub} OK"
 
     if (cmd == "pause"):
@@ -111,14 +122,14 @@ def cmd_handler(msg: str) -> str:
             return "R pause ERR too many arguments"
         else:
             if args[0].upper() == "ON":
-                if STREAMS.paused():
+                if ctx.paused:
                     return "R pause ERR already paused"
-                STREAMS.pause_all()
+                ctx.paused = True
                 return "R pause ON"
             elif args[0].upper() == "OFF":
-                if not STREAMS.paused():
+                if not ctx.paused:
                     return "R pause ERR not paused"
-                STREAMS.resume_all()
+                ctx.paused = False
                 return "R pause OFF"
             else:
                 return "R pause ERR wrong argument"
@@ -127,23 +138,32 @@ def cmd_handler(msg: str) -> str:
 
 
 async def ee4_srv(r: StreamReader, w: StreamWriter):
-    thread = Thread(target=STREAMS.new_streaming_loop, args=(w,))
-    thread.start()
-    while True:
-        try:
+    logger.info(f"connection established")
+    ctx = SubscriptionContext()
+    ctx_thread = Thread(target=ctx.create_streaming_loop, args=(w,))
+    ctx_thread.start()
+    try:
+        while not r.exception():
             msg_raw = await r.readline()
             msg = msg_raw.decode().strip()
             logger.info(f"< {msg}")
-            out = cmd_handler(msg)
+            out = cmd_handler(msg, ctx)
             logger.info(f"> {out}")
             w.write(f"{out}\r\n".encode())
             await w.drain()
-        except UnicodeDecodeError:
-            logger.warn("invalid char found")
-            break
-        except AttributeError:
-            logger.warn("attribute error found")
-            break
+    except UnicodeDecodeError:
+        logger.warning("invalid char encountered")
+    except AttributeError:
+        logger.warning("attribute error encountered")
+    except ConnectionError:
+        logger.warning("connection error encountered")
+    finally:
+        logger.info("cleaning up resources")
+        ctx.end()
+        ctx_thread.join()
+        del ctx
+        del ctx_thread
+    logger.info(f"resources purged. connection terminated.")
 
 
 async def main():
@@ -154,13 +174,10 @@ async def main():
         async with srv:
             await srv.serve_forever()
     except KeyboardInterrupt:
+        await srv.wait_closed()
         exit(0)
 
-# For testing...
 if (__name__ == "__main__"):
-    # Global to handle streaming
-    STREAMS = StreamHolder()
-    STREAM_TYPES = ["acc", "bvp", "gsr", "ibi", "tmp", "bat", "tag", "hr"]
     # In try-except to suppress irrelevant errors
     try:
         asyncio.run(main(), debug=True)
